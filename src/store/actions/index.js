84534415bb6ce4f1724b6812b5e0f81536f77ea0
papadopan <antonios.papadopan@gmail.com>
2019-06-30 13:38:26 +0200

    vaggelis added

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -8,7 +8,7 @@ import axios from 'axios';
  export const ERROR = 'ERROR';
  export const UPDATE = 'UPDATE';
  export const SELECTED_COUNTRY = 'SELECTED_COUNTRY';
- export const ANTONIOS = 'ANTONIOS';
+ export const ANTONIOS = 'VAGGELIS';
 
  /**
   * action functions
